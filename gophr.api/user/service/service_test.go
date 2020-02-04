e6983fe300a6a1436ca6ee30ed0705919ef3399f
Jayson Vibandor <danv@isell.com.au>
2020-02-04 01:43:30 +0000

    added more tests

--- gophr.api/user/service/service_test.go
+++ gophr.api/user/service/service_test.go
@@ -61,6 +61,30 @@ func TestService_GetByEmail(t *testing.T) {
 	})
 }
 
-func TestService_GetByUsername(t *testing.T) {}
+func TestService_GetByUsername(t *testing.T) {
+	t.Run("Existing username should return its equavalent user object", func(t *testing.T){
+		repo := new(mocks.Repository)
+		want := &user.User{
+			ID:       "12345",
+			Username: "luffy.monkey",
+			Email:    "luffy.monkey@gmail.com",
+		}
+		repo.On("GetByUsername", mock.Anything, mock.AnythingOfType("string")).Return(want, nil).Once()
+		svc := New(repo)
+		got, _ := svc.GetByUsername(context.Background(), "luffy.monkey")
+		assert.Equal(t, want, got)
+	})
 
-func TestService_Save(t *testing.T) {}
+	t.Run("Not existing username should return a ErrNotFound error", func(t *testing.T) {
+		repo := new(mocks.Repository)
+		want := errors.ErrorNotFound
+		repo.On("GetByUsername", mock.Anything, mock.AnythingOfType("string")).Return(nil, errors.ErrorNotFound)
+		svc := New(repo)
+		_, got := svc.GetByUsername(context.Background(), "luffy.monkey")
+		assert.Equal(t, want, got)
+	})
+
+}
+
+func TestService_Save(t *testing.T) {
+}
