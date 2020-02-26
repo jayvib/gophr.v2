0b3dc8cc398fb382184b9642c44bbd10757abc27
jayvib <jayson.vibandor@gmail.com>
2020-02-26 21:26:21 +0800

    delete more files

--- gophr.client/gophr.api/errors.go
+++
@@ -1,9 +0,0 @@
-package gophr
-
-import "errors"
-
-var (
-	ErrorNotFound           = errors.New("item not found")
-	ErrorSessionExpired     = errors.New("session expired")
-	ErrorInvalidCredentials = errors.New("invalid credentials")
-)
