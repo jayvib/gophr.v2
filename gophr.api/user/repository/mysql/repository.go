a76fbe0ea5d756666885ae99f45506e9ec63c49d
jayvib <jayson.vibandor@gmail.com>
2020-03-06 13:35:58 +0800

    Update refactor to improve error checking

--- gophr.api/user/repository/mysql/repository.go
+++ gophr.api/user/repository/mysql/repository.go
@@ -25,35 +25,44 @@ type Repository struct {
 func (r *Repository) GetByID(ctx context.Context, id string) (*user.User, error) {
 	return nil, nil
 }
-func (r *Repository) GetByEmail(ctx context.Context, email string) (*user.User, error) {
+func (r *Repository) GetByEmail(ctx context.Context, email string) (u *user.User, err error) {
 	query := "SELECT id,userId,username,email,password,created_at,updated_at,deleted_at FROM user WHERE email = ?"
 	row, err := r.conn.QueryContext(ctx, query, email)
 	if err != nil {
 		log.Debug(err)
-		var cerr error
-		switch err {
-		case sql.ErrNoRows:
-			cerr = ErrNotFound
-		default:
-			cerr = fmt.Errorf("mysql: unexpected error %w", err)
-		}
+		cerr := r.checkError(err)
 		return nil, cerr
 	}
 	defer func() {
 		// TODO: Handle the error
-		_ = row.Close()
+		if e := row.Close(); e != nil {
+			err = e
+		}
 	}()
 
-	var u user.User
+	u = new(user.User)
 	for row.Next() {
-		// TODO: Handle the error
-		_ = row.Scan(&u.ID, &u.UserID, &u.Username, &u.Email, &u.Password, &u.CreatedAt, &u.UpdatedAt, &u.DeletedAt)
+		err = row.Scan(&u.ID, &u.UserID, &u.Username, &u.Email, &u.Password, &u.CreatedAt, &u.UpdatedAt, &u.DeletedAt)
+		if err != nil {
+			return nil, r.checkError(err)
+		}
 	}
 	if err = row.Err(); err != nil {
 		log.Debug(err)
-		return nil, err
+		return nil, r.checkError(err)
 	}
-	return &u, nil
+	return u, nil
+}
+
+func (r *Repository) checkError(err error) error {
+	var cerr error
+	switch err {
+	case sql.ErrNoRows:
+		cerr = ErrNotFound
+	default:
+		cerr = fmt.Errorf("mysql: unexpected error %w", err)
+	}
+	return cerr
 }
 func (r *Repository) GetByUsername(ctx context.Context, uname string) (*user.User, error) {
 	return nil, nil
