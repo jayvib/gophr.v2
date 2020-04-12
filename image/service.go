4de817f393bc900d083577bc43c6962aace3ce0a
Jayson Vibandor <danv@isell.com.au>
2020-04-12 02:41:39 +0100

    created a mock objects for the image domain

--- image/service.go
+++ image/service.go
@@ -1,1 +1,12 @@
 package image
+
+import "context"
+
+//go:generate mockery --name=Service
+
+type Service interface {
+	Save(ctx context.Context, image *Image) error
+	Find(ctx context.Context, id string) (*Image, error)
+	FindAll(ctx context.Context, offset int) ([]*Image, error)
+	FindAllByUser(ctx context.Context, userId string, offset int) ([]*Image, error)
+}
---
+++ image/service/service.go
@@ -0,0 +1,31 @@
+package service
+
+import (
+	"context"
+	"gophr.v2/image"
+)
+
+func New(repo image.Repository) image.Service {
+	return &service{repo: repo}
+}
+
+type service struct {
+	repo image.Repository
+}
+
+func (s *service) Save(ctx context.Context, image *image.Image) error {
+	return nil
+}
+
+func (s *service) Find(ctx context.Context, id string) (*image.Image, error) {
+	return nil, nil
+}
+
+func (s *service) FindAll(ctx context.Context, offset int) ([]*image.Image, error) {
+	return nil, nil
+}
+
+func (s *service) FindAllByUser(ctx context.Context, userId string, offset int) ([]*image.Image, error) {
+	return nil, nil
+}
+
