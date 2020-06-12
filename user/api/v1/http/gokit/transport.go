9e23a9f6e42bf2fdaadf56d2819ed3ac15985f81
jaysonv <jayson.vibandor@gmail.com>
2020-06-12 12:52:26 +0800

    move structs to transport.go file

--- user/api/v1/http/gokit/transport.go
+++ user/api/v1/http/gokit/transport.go
@@ -65,3 +65,47 @@ func MakeDeleteEndpoint(svc user.Service) endpoint.Endpoint {
 		return deleteResponse{}, nil
 	}
 }
+
+type getByUserIDRequest struct {
+	UserID string `json:"userID"`
+}
+
+type getByUserIDResponse struct {
+	User  *user.User `json:"user,omitempty"`
+	Error string     `json:"error,omitempty"`
+}
+
+type getAllRequest struct {
+	Cursor string `json:"cursor"`
+	Number int    `json:"num"`
+}
+
+type getAllResponse struct {
+	Users      []*user.User `json:"users,omitempty"`
+	NextCursor string       `json:"next_cursor,omitemptY"`
+	Error      string       `json:"error,omitempty"`
+}
+
+type registerRequest struct {
+	*user.User
+}
+
+type registerResponse struct {
+	Error string `json:"error,omitempty"`
+}
+
+type updateRequest struct {
+	*user.User
+}
+
+type updateResponse struct {
+	Error string `json:"error,omitempty"`
+}
+
+type deleteRequest struct {
+	ID string `json:"id"`
+}
+
+type deleteResponse struct {
+	Error string `json:"error,omitempty"`
+}
